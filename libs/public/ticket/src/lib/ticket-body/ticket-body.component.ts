785a67757653496ce7ebdc24798259b1807b45bf
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-03 20:29:59 +0200

    💄 Small bug fix

--- libs/public/ticket/src/lib/ticket-body/ticket-body.component.ts
+++ libs/public/ticket/src/lib/ticket-body/ticket-body.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './ticket-body.component.html',
   styleUrls: ['./ticket-body.component.scss'],
 })
-export class TicketBodyComponent implements OnInit {
-  constructor() {}
+export class TicketBodyComponent{
+  // constructor() {}
 
-  ngOnInit(): void {}
+  // ngOnInit(): void {}
 }

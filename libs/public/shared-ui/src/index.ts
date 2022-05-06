3291e9396cdfd70742611a699350296c91654a62
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-06 19:49:34 +0200

    💄 (UI) : Added Navbar component

--- libs/public/shared-ui/src/index.ts
+++ libs/public/shared-ui/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/public-shared-ui.module';
+
+export * from './lib/nav-bar/nav-bar.component';
---
+++ libs/public/shared-ui/src/lib/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grid-watch-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+})
+export class NavBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

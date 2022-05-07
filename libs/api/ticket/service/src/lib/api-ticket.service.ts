4f13a48bb25bd2595fc8f4288426f2b6bd17cead
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-07 13:34:04 +0200

    🤡 (Service): Create Ticket mock function

--- libs/api/ticket/service/src/lib/api-ticket.service.ts
+++ libs/api/ticket/service/src/lib/api-ticket.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ApiTicketService {}
+export class ApiTicketService {
+
+
+    // createTicket(user: User ,issue_type : string, description: string, imgUrl: string, location: MapLocation): boolean
+    createTicket(issue_type : string, description: string, imgUrl: string): boolean
+    {
+        console.log('Issue type: ' + issue_type);
+        console.log('description: ' + description);
+        console.log('img Url: ' + imgUrl);
+        return true;
+    }
+}

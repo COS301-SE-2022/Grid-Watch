811a947d4ecfd9069cd4b2cd2a04611014a92a66
Joshua Young <u20442018@up.ac.za>
2022-05-07 14:53:12 +0200

    ✨ (api): Added simple controller endpoints

--- libs/api/ticket/api/src/lib/api-ticket-api-controller.controller.ts
+++ libs/api/ticket/api/src/lib/api-ticket-api-controller.controller.ts
@@ -1,11 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+  } from '@nestjs/common';
+
+
 
 @Controller('ticket')
 export class TicketController {
 
     @Get()
-    tickets(){
-        return  "Tickets lets gooo";
+    testing(){
+        return  "Testing Tickets";
     }
+
+    // @Get()
+    // async findAll(): Promise<Ticket> {
+    //   return this.ticketService.findAll();
+    // }
+  
+    // @Get(':id')
+    // async find(@Param('id') id: number): Promise<Ticket> {
+    //   return this.ticketService.find(id);
+    // }
+  
+    // @Post()
+    // async create(@Body('item') ticket: Ticket): Promise<void> {
+    //   this.ticketService.create(ticket);
+    // }
+  
+    // @Put()
+    // async update(@Body('item') ticket: Ticket): Promise<void> {
+    //   this.ticketService.update(ticket);
+    // }
+  
+    // @Delete(':id')
+    // async delete(@Param('id') id: number): Promise<void> {
+    //   this.ticketService.delete(id);
+    // }
 
 }

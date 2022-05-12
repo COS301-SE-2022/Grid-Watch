f840a674294f9f6b0e8e6689a92f52945d345157
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-12 13:51:06 +0200

    🐛 (UI) : Fixed bug

--- libs/public/ticket/src/lib/edit-ticket/edit-ticket.component.spec.ts
+++ libs/public/ticket/src/lib/edit-ticket/edit-ticket.component.spec.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { RouterModule } from '@nestjs/core';
 import { RouterTestingModule } from "@angular/router/testing";
 import { EditTicketComponent } from './edit-ticket.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('EditTicketComponent', () => {
   let component: EditTicketComponent;
@@ -11,7 +12,7 @@ describe('EditTicketComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EditTicketComponent],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule, HttpClientModule]
     }).compileComponents();
   });
 

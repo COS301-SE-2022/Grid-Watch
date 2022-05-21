b0eb5ce1bc7e66d039d92714b16b43fe2ae88eb4
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-21 13:54:19 +0200

    🍱 (UI) : Add assets to tech team application

--- apps/tech-team/src/app/app.module.ts
+++ apps/tech-team/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedUiModule } from '@grid-watch/shared-ui';
+import { HomePageBodyComponent } from '@grid-watch/public/home-page';
+
+const routes: Routes = [
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageBodyComponent },
+  // { path: 'tickets', component: TicketBodyComponent },
+  // { path: 'createTicket', component: CreateTicketComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    SharedUiModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

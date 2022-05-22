62ccb83558f512afb59c6606454543a11523af4e
Joshua Young <u20442018@up.ac.za>
2022-05-22 19:49:10 +0200

    ✨ (api): Created Tech Team dto

---
+++ libs/api/profiles/tech-team/api/src/lib/dto/api-profiles-tech-team-api.dto.ts
@@ -0,0 +1,36 @@
+import {
+    IsNotEmpty,
+    IsNumber, 
+    IsOptional, 
+    IsString, 
+} from 'class-validator';
+
+export class ApiProfilesTechTeamApiDto {
+@IsNumber()
+@IsNotEmpty()
+id! : number;
+
+@IsString()
+@IsNotEmpty()
+name! : string;
+
+@IsString()
+@IsOptional()
+email! : string;
+
+@IsString()
+@IsNotEmpty()
+specialisation! : string;
+
+@IsNumber()
+@IsNotEmpty()
+contact_number!  : number;
+
+@IsNumber()
+@IsOptional()
+nr_jobs_completed! : number;
+
+@IsNumber()
+@IsOptional()
+rating_of_jobs! : number;
+}

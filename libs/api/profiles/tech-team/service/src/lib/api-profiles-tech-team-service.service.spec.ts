4aecf47610481d16802850778fa84e296ec775f6
Thebe <u18371435@tuks.co.za>
2022-05-25 16:24:09 +0200

    ✨ (api): Added services for Tech Team

--- libs/api/profiles/tech-team/service/src/lib/api-profiles-tech-team-service.service.spec.ts
+++ libs/api/profiles/tech-team/service/src/lib/api-profiles-tech-team-service.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiProfilesTechTeamServiceService } from './api-profiles-tech-team-service.service';
+import {CommandBus} from '@nestjs/cqrs';
 
 describe('ApiProfilesTechTeamServiceService', () => {
   let service: ApiProfilesTechTeamServiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiProfilesTechTeamServiceService],
+      providers: [ApiProfilesTechTeamServiceService,CommandBus],
     }).compile();
 
     service = module.get<ApiProfilesTechTeamServiceService>(

2268c633faf82ca4af581297b4026a3ef72dcaec
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-27 14:32:39 +0200

    💄 (UI) File upload functionality on Edit Ticket

--- libs/public/ticket/src/lib/edit-ticket/edit-ticket.component.ts
+++ libs/public/ticket/src/lib/edit-ticket/edit-ticket.component.ts
@@ -14,11 +14,19 @@ export class EditTicketComponent implements OnInit {
   default_upload! : string | null;
   @Input() issue_type! : string;
   @Input() other_details! : string;
+  file! : File;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
 
 
   @Input() ticket! : TicketDto;
   updateURL = "http://localhost:3333/api/ticket/update/";
   getTicketURL = "http://localhost:3333/api/ticket/";
+  uploadURL = "http://localhost:3333/api/ticket/upload";
 
   constructor(
       private route: ActivatedRoute, 
@@ -29,24 +37,23 @@ export class EditTicketComponent implements OnInit {
 
   ngOnInit(): void {
     this.ticket = new TicketDto();
+
+    //User Data
     this.default_upload = "assets/upload-solid.svg"
     this.display_name = "John Doe";
+
+    //Get parameters
     const temp = this.route.snapshot.paramMap.get('id');
+
+    //Add parameter to URL
     this.getTicketURL = this.getTicketURL + temp
     this.updateURL = this.updateURL + temp
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json'
-      })
-    };
     
-    this.http.get<TicketDto[]>(this.getTicketURL, httpOptions).subscribe(
+    this.http.get<TicketDto[]>(this.getTicketURL, this.httpOptions).subscribe(
       (data) =>
       {
-        console.log(data[0]);
-        this.initialiseFields(data[0]);
         this.ticket = data[0];
+        this.initialiseFields(data[0]);
       }
     )
   }
@@ -54,14 +61,15 @@ export class EditTicketComponent implements OnInit {
   fileUploaded(e: any)
   {
 
-    const file = e.target.files[0];
+    this.file  = e.target.files[0];
 
 
     const reader = new FileReader();
     reader.onload = () => {
       this.default_upload = reader.result as string;
     }
-    reader.readAsDataURL(file)
+    reader.readAsDataURL(this.file)
+     
   }
 
   editTicket() : void
@@ -74,25 +82,53 @@ export class EditTicketComponent implements OnInit {
     this.ticket.ticket_create_date = new Date();
     this.ticket.ticket_upvotes = 0;
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json'
-      })
-    };
     
-    this.http.put<TicketDto>(this.updateURL, this.ticket, httpOptions).subscribe(
-      (data) => {
-        console.log("success!");
-        
+    const formData = new FormData();
+    formData.append("photo", this.file, this.file.name);
+
+    this.http.post<Express.Multer.File>(this.uploadURL, formData)
+    .subscribe({
+      next: (data) => {
         console.log(data);
-        this.router.navigateByUrl("/tickets")
+        
+        this.updateTicket(data.filename);
+        this.showSuccessMessage();
+        // this.router.navigateByUrl("/tickets")
+      },
+      error: error => {
+        this.showErrorMessage();
+        this.updateTicket(this.ticket.ticket_img);
       }
-    )
+    }) 
+  }
+  
+  updateTicket(link : string) {
+    this.ticket.ticket_img = link
+    this.http.put<TicketDto>(this.updateURL, this.ticket, this.httpOptions).subscribe(
+        () => {
+          if (this.file != null)
+            console.log("Successs");
+        },
+        () => {
+          this.showErrorMessage();
+        }
+      ) 
+  }
+
+
+  showErrorMessage() {
+    alert("Error editing the ticket");
+  }
+  
+  showSuccessMessage() {
+    alert("Sucessfully editted the ticket");
   }
 
   initialiseFields(data : TicketDto)
   {
     
+    if (this.ticket.ticket_img != "")
+        this.default_upload = "assets/" + this.ticket.ticket_img;
     if ((data.ticket_type === "Pothole") || (data.ticket_type === "Water Outage") ||
         (data.ticket_type === "Sinkhole")  || (data.ticket_type === "Electricity Outage") ) 
           this.issue_type = data.ticket_type;

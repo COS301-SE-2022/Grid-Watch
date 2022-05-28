aabfe89f64347572ea6f8f83a35a46689056aa83
Joshua Young <u20442018@up.ac.za>
2022-05-28 18:41:55 +0200

    ✨ (api): Linked controller and service layer

--- libs/api/profiles/tech-team/api/src/lib/controllers/api-profiles-tech-team-api-controller.controller.ts
+++ libs/api/profiles/tech-team/api/src/lib/controllers/api-profiles-tech-team-api-controller.controller.ts
@@ -35,49 +35,42 @@ export class ApiProfilesTechTeamApiControllerController {
     //update Tech Team Name endpoint
     @Put('/update/name/:id')
     async UpdateTechTeamName(@Param() params,@Body() name: string):Promise<boolean> {
-        //return this.apiTechTeamService.updateTechTeamName(parseInt(params.id),name["name"]);
-        return true;
+        return this.apiTechTeamService.updateTechTeamName(parseInt(params.id),name["name"]);
     }
 
     //update Tech Team email endpoint
     @Put('/update/email/:id')
     async UpdateTechTeamEmail(@Param() params,@Body() email: string):Promise<boolean> {
-        //return this.apiTechTeamService.updateTechTeamEmail(parseInt(params.id),email["email"]);
-        return true;
+        return this.apiTechTeamService.updateTechTeamEmail(parseInt(params.id),email["email"]);
     }
 
     //update Tech Team specialisation endpoint
     @Put('/update/specialisation/:id')
     async UpdateTechTeamSpecialisation(@Param() params,@Body() specialisation: string):Promise<boolean> {
-        //return this.apiTechTeamService.updateTechTeamSpecialisation(parseInt(params.id),specialisation["specialisation"]);
-        return true;
+        return this.apiTechTeamService.updateTechTeamSpecialisation(parseInt(params.id),specialisation["specialisation"]);
     }
 
     //update Tech Team contact number endpoint
     @Put('/update/contactnr/:id')
     async UpdateTechTeamContactNr(@Param() params,@Body() ContactNr: string):Promise<boolean> {
-        //return this.apiTechTeamService.updateTechTeamContactNr(parseInt(params.id),ContactNr["ContactNr"]);
-        return true;
+        return this.apiTechTeamService.updateTechTeamContactNr(parseInt(params.id),ContactNr["ContactNr"]);
     }
 
     //update Tech Team number jobs completed endpoint
     @Put('/update/nrjobscompleted/:id')
     async UpdateTechTeamNrJobsCompleted(@Param() params,@Body() nrJobsCompleted: number):Promise<boolean> {
-        //return this.apiTechTeamService.updateTechTeamNrJobsCompleted(parseInt(params.id),nrJobsCompleted["nrJobsCompleted"]);
-        return true;
+        return this.apiTechTeamService.updateTechTeamNrJobsCompleted(parseInt(params.id),nrJobsCompleted["nrJobsCompleted"]);
     }
 
     //Increment Tech Team number jobs completed endpoint
     @Put('/update/incjobscompleted/:id')
     async IncTechTeamNrJobsCompleted(@Param() params):Promise<boolean> {
-        //return this.apiTechTeamService.incTechTeamNrJobsCompleted(parseInt(params.id));
-        return true;
+        return this.apiTechTeamService.IncTechTeamNrJobsCompleted(parseInt(params.id));
     }
 
     //update Tech Team rating endpoint
     @Put('/update/ratingjobs/:id')
     async UpdateTechTeamRatingJobs(@Param() params,@Body() ratingJobs: number):Promise<boolean> {
-        //return this.apiTechTeamService.updateTechTeamRatingJobs(parseInt(params.id),ratingJobs["ratingJobs"]);
-        return true;
+        return this.apiTechTeamService.updateTechTeamRatingJobs(parseInt(params.id),ratingJobs["ratingJobs"]);
     }
 }

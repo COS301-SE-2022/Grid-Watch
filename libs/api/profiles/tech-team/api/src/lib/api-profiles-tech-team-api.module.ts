7aaed334cc8c3fdf41d9ae6d2130e7008ca4065b
Joshua Young <u20442018@up.ac.za>
2022-05-28 19:53:56 +0200

    🐛 (api): Fix tech Team service layer import

--- libs/api/profiles/tech-team/api/src/lib/api-profiles-tech-team-api.module.ts
+++ libs/api/profiles/tech-team/api/src/lib/api-profiles-tech-team-api.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApiProfilesTechTeamApiControllerController } from './controllers/api-profiles-tech-team-api-controller.controller';
-import { ApiProfilesAdminServiceModule } from '@grid-watch/api/profiles/admin/service';
+import { ApiProfilesTechTeamServiceModule } from '@grid-watch/api/profiles/tech-team/service';
 
 
 @Module({
   controllers: [ApiProfilesTechTeamApiControllerController],
-  imports: [ApiProfilesAdminServiceModule],
+  imports: [ApiProfilesTechTeamServiceModule],
   providers: [],
   exports: [],
 })

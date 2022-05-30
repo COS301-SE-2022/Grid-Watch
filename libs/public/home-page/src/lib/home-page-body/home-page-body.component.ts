f438a4a0d76299d2fe1a1ccd8cb7f19c243c2266
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-30 20:05:14 +0200

    ✨(UI): Added google maps feature

--- libs/public/home-page/src/lib/home-page-body/home-page-body.component.ts
+++ libs/public/home-page/src/lib/home-page-body/home-page-body.component.ts
@@ -8,9 +8,74 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageBodyComponent implements OnInit{
 
   application_type! : string | undefined;
-
+  
+  map!: google.maps.Map
+  infoWindow!: google.maps.InfoWindow;
+  
   ngOnInit() : void {
     const temp = document.getElementById("application_type");
     this.application_type = temp?.innerHTML;
+    this.initMap();
   }
+
+  // Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+initMap(): void {
+  this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -25.7487, lng: 28.2380 },
+    zoom: 10,
+  });
+  this.infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+
+  this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+
+            this.infoWindow.setPosition(pos);
+            this.infoWindow.setContent("Location found.");
+            this.infoWindow.open(this.map);
+            this.map.setCenter(pos);
+          },
+          () => {
+            // this.handleLocationError(true, this.infoWindow, this.map.getCenter()!);
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.handleLocationError(false, this.infoWindow, this.map.getCenter()!);
+      }
+    });
+  }
+
+  handleLocationError(
+  browserHasGeolocation: boolean,
+  infoWindow: google.maps.InfoWindow,
+  pos: google.maps.LatLng
+  ) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(this.map);
+  }
+
 }
--- libs/public/home-page/src/lib/public-home-page.module.ts
+++ libs/public/home-page/src/lib/public-home-page.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomePageBodyComponent } from './home-page-body/home-page-body.component';
+import { GoogleMapsModule } from '@angular/google-maps'
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @NgModule({
   imports: [
@@ -10,6 +13,8 @@ import { HomePageBodyComponent } from './home-page-body/home-page-body.component
     RouterModule.forChild([
        {path: 'home', pathMatch: 'full', component: HomePageBodyComponent} 
     ]),
+    BrowserModule,
+    GoogleMapsModule
   ],
   declarations: [HomePageBodyComponent],
   exports: [HomePageBodyComponent],

04008c3ba63c929b845a81eadf90111c585d4a84
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-05-30 15:00:26 +0200

    ⏪ Revert chanegs for future PR

--- libs/tech-team/profile/src/lib/register-tech-team/register-tech-team.component.ts
+++ libs/tech-team/profile/src/lib/register-tech-team/register-tech-team.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input} from '@angular/core';
 import { Router } from '@angular/router';
+// import { ApiProfilesTechTeamApiDto } from '/libs/api/profiles/tech-team/api/shared/techteamdto/src/lib/api-profiles-tech-team-api-shared-techteamdto';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { ApiProfilesTechTeamApiDto } from 'libs/api/profiles/tech-team/api/src/lib/dto/api-profiles-tech-team-api.dto';
 
 @Component({
   selector: 'grid-watch-register-tech-team',
@@ -10,14 +10,14 @@ import { ApiProfilesTechTeamApiDto } from 'libs/api/profiles/tech-team/api/src/l
 })
 export class RegisterTechTeamComponent {
 
-  @Input() company_name! : string;
-  @Input() issue_types! : string[];
+  @Input() name! : string;
+  @Input() specialisation! : string[];
   @Input() email! : string;
   @Input() contact_number! : string;
   @Input() password! : string;
   @Input() confirm_password! : string;
 
-  tech_profile : ApiProfilesTechTeamApiDto = new ApiProfilesTechTeamApiDto();
+  // tech_profile : ApiProfilesTechTeamApiDto = new ApiProfilesTechTeamApiDto();
 
   issue_options = ["Pothole", "Sinkhole", "Water Outage", "Electricity Outage", "Other"];
   
@@ -28,7 +28,7 @@ export class RegisterTechTeamComponent {
 
   register() : void {
     console.log("register");
-    console.log(this.tech_profile);
+    // console.log(this.tech_profile);
     
     
   }

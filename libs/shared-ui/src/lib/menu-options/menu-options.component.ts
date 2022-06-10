f65441fc530597d5c118c240a0d6a245528ae614
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-06-10 15:15:09 +0200

    💄(UI) : Added menu options styling

--- libs/shared-ui/src/lib/menu-options/menu-options.component.ts
+++ libs/shared-ui/src/lib/menu-options/menu-options.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-options.component.scss'],
 })
 export class MenuOptionsComponent implements OnInit {
+
+  applicationType! : string | undefined;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const temp = document.getElementById("application_type");
+    this.applicationType = temp?.innerHTML;
+  }
 }

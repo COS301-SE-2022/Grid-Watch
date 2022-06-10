8fe67529c019c16f51e38dacb8365c22dcf967c0
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-06-10 04:51:23 +0200

    ✅ (integration): Added some integration testing

--- apps/public-e2e/src/integration/app.spec.ts
+++ apps/public-e2e/src/integration/app.spec.ts
@@ -11,3 +11,46 @@ describe('public', () => {
   //   getGreeting().contains('Welcome public');
   // });
 });
+
+///////////////////////////////////////////////
+///////////// Integration Testing /////////////
+///////////////////////////////////////////////
+
+describe('Ticket controller Integration Testing',()=>{
+  
+  it('Test getTicket request',()=>{
+    cy.request("http://localhost:3333/api/ticket/1")
+      .then((response)=>{
+        expect(response.body).to.have.property("ticketId",1);
+      })
+  })
+
+  it('Test incUpvotes',()=>{
+    cy.request("http://localhost:3333/api/ticket/inc/1")
+      .then((response)=>{
+        expect(response.body).to.have.property("upvotes",2);
+      })
+  })
+
+  it('Test getCity',()=>{
+    cy.request("http://localhost:3333/api/ticket/city/Pretoria")
+      .then((response)=>{
+        expect(response.body).to.have.property("city","Pretoria");
+      })
+  })
+
+  it('Test getStatus',()=>{
+    cy.request("http://localhost:3333/api/ticket/status/dispached")
+      .then((response)=>{
+        expect(response.body).to.have.property("status","dispached");
+      })
+  })
+
+  it('Test issue',()=>{
+    cy.request("http://localhost:3333/api/ticket/issue/potholes")
+      .then((response)=>{
+        expect(response.body).to.have.property("issue","potholes");
+      })
+  })
+
+})

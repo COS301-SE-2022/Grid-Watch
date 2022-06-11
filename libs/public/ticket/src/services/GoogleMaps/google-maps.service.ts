e771f889e3c4843c02199caa4317fffbab862120
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-06-11 16:12:05 +0200

    🎨(UI): Create google maps service

---
+++ libs/public/ticket/src/services/GoogleMaps/google-maps.service.ts
@@ -0,0 +1,23 @@
+import { temporaryAllocator } from '@angular/compiler/src/render3/view/util';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleMapsService {
+
+  constructor() { }
+
+  public async getLocation(placeID : string) : Promise<string>  {
+    const geocoder = new google.maps.Geocoder();
+    // const output;
+    const temp = await geocoder.geocode({placeId : placeID}).then(
+      ({results}) => {
+        return results[0].formatted_address;
+      }
+    )
+    return temp
+}
+
+  
+}

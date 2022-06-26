3f509d940e7499dcf903068c88afac2ed4035b33
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-06-26 11:34:04 +0200

    ♻️ (api): Refactor Tech Team controller

--- libs/api/profiles/tech-team/api/shared/techteamdto/src/lib/api-profiles-tech-team-api-shared-techteamdto.spec.ts
+++ libs/api/profiles/tech-team/api/shared/techteamdto/src/lib/api-profiles-tech-team-api-shared-techteamdto.spec.ts
@@ -1,7 +1,7 @@
-import { techTeamDto } from './api-profiles-tech-team-api-shared-techteamdto';
+import { TechTeamDto } from './api-profiles-tech-team-api-shared-techteamdto';
 
 describe('ApiProfilesTechTeamApiDto', () => {
   it('should be defined', () => {
-    expect(new techTeamDto()).toBeDefined();
+    expect(new TechTeamDto()).toBeDefined();
   });
 });

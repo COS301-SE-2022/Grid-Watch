916becd968ac479b67be9d25bfa01609ce51c1fd
anru123 <anrunel.an@gmail.com>
2022-07-07 18:48:10 +0200

    🗃️ (db): Add CRUD operations to user model

--- libs/api/profiles/public/repository/src/lib/api-profiles-public-repository-data-access.ts
+++ libs/api/profiles/public/repository/src/lib/api-profiles-public-repository-data-access.ts
@@ -9,46 +9,83 @@ export class ApiProfilesPublicRepositoryDataAccess{
 
     async createUser(userDto:UserDto){
 
-    if(!userDto.name)
-        throw Error("name_falsy");
-    if(!userDto.email)
-        throw Error("email_falsy");
-    if(!userDto.password)
-        throw Error("password_falsy");
+        if(!userDto.name)
+            throw Error("name_falsy");
+        if(!userDto.email)
+            throw Error("email_falsy");
+        if(!userDto.password)
+            throw Error("password_falsy");
 
-    const salt = await this.bcrypt.genSalt(6);
-    const hash = await this.bcrypt.hash(userDto.password, salt)
+        const salt = await this.bcrypt.genSalt(6);
+        const hash = await this.bcrypt.hash(userDto.password, salt)
 
-    const user = await this.prisma.user.create({
-        data:
-        {
-            name :                  userDto.name,
-            email :                 userDto.email,
-            password :              hash,
-            passwordSalt:           salt,
-            created :               new Date()
+        const user = await this.prisma.user.create({
+            data:
+            {
+                name :                  userDto.name,
+                email :                 userDto.email,
+                password :              hash,
+                passwordSalt:           salt,
 
-        },
-    });
+            },
+        });
 
     return user
     }
 
-    async getUserName(userId: number){
+    async getUser(userId: number){
 
-        const user = await this.prisma.techTeam.findMany({
+        const user = await this.prisma.user.findMany({
 
             where:{
-                name : name,
+                id : userId,
             },
 
         })
 
-        if (techTeam) {
-            return techTeam;
+        if (user) {
+            return user;
         }
         else{
-            return "Techteam " + name + " not found!";
+            return "User with ID: " + userId + " not found!";
+        }
+        
+    }
+
+    async getUserName(userName: string){
+
+        const user = await this.prisma.user.findMany({
+
+            where:{
+                name : userName,
+            },
+
+        })
+
+        if (user) {
+            return user;
+        }
+        else{
+            return "Username: " + userName + " not found!";
+        }
+        
+    }
+
+    async getUserEmail(userEmail: string){
+
+        const user = await this.prisma.user.findMany({
+
+            where:{
+                email : userEmail,
+            },
+
+        })
+
+        if (user) {
+            return user;
+        }
+        else{
+            return "User email: " + userEmail + " not found!";
         }
         
     }
@@ -93,4 +130,47 @@ async updatePassword(techTeamId: number, newPassword: string){
             },
         });
     }
+}
+
+async updateUser(userId:number,userDto:UserDto){
+
+    await this.prisma.user.update({
+        where:{
+            id : userId,
+        },
+        data:
+        {
+            name :                  techTeamDto.name,
+            email :                 techTeamDto.email,
+            specialisation :        techTeamDto.specialisation,
+            contactNumber :         techTeamDto.contactNumber,
+        },
+    });
+}
+
+
+async updateEmail(userId:number,userEmail:string){
+
+    await this.prisma.user.update({
+        where:{
+            id : userId,
+        },
+        data:
+        {
+            name :            userEmail,
+        },
+    });
+}
+
+async updateName(userId:number,userName:string){
+
+    await this.prisma.user.update({
+        where:{
+            id : userId,
+        },
+        data:
+        {
+            name :              userName,
+        },
+    });
 }

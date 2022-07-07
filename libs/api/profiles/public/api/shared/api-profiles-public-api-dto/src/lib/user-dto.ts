6249b64a0a8063d7252adf26715229a9b648285f
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-07 17:33:10 +0200

    🎨 (api): Finalize user-dto

--- libs/api/profiles/public/api/shared/api-profiles-public-api-dto/src/lib/user-dto.ts
+++ libs/api/profiles/public/api/shared/api-profiles-public-api-dto/src/lib/user-dto.ts
@@ -1,41 +1,26 @@
 import {
   IsNotEmpty,
-  IsNumber, 
-  IsOptional, 
   IsString, 
   IsEmail,
-  IsPhoneNumber,
   IsAlphanumeric,
+  IsDate,
 } from 'class-validator';
 
 export class UserDto {
 
-@IsString()
-@IsNotEmpty()
-name! : string;
-
 @IsEmail()
-@IsOptional()
+@IsNotEmpty()
 email! : string;
-
-@IsString()
-@IsNotEmpty()
-specialisation! : string;
-
-@IsPhoneNumber()
-@IsNotEmpty()
-contactNumber!  : string;
-
-@IsNumber()
-@IsOptional()
-nrJobsCompleted! : number;
-
-@IsNumber()
-@IsOptional()
-ratingOfJobs! : number;
 
 @IsAlphanumeric()
 @IsNotEmpty()
 password! : string;
 
+@IsString()
+@IsNotEmpty()
+name! : string;
+
+@IsDate()
+@IsNotEmpty()
+dateCreated! : Date;
 }

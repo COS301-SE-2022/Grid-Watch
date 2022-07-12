f8643c6c390a214911c16018d74cc2206cc7ed4b
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-12 19:52:46 +0200

    ✨ (UI) : Created menu-bar component

--- libs/tech-team/general-ui/src/index.ts
+++ libs/tech-team/general-ui/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/tech-team-general-ui.module';
+
+export * from './lib/menu-bar/menu-bar.component';
--- libs/tech-team/general-ui/src/lib/tech-team-general-ui.module.ts
+++ libs/tech-team/general-ui/src/lib/tech-team-general-ui.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   imports: [
@@ -10,5 +11,7 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [MenuBarComponent],
+  exports: [MenuBarComponent],
 })
 export class TechTeamGeneralUiModule {}

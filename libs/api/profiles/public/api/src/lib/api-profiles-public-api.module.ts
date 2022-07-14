e9e38cb18a09817b664c466540acc21b51633cc0
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-14 20:41:02 +0200

    🐛 (api) : Fixed issue with dependencies

--- libs/api/profiles/public/api/src/lib/api-profiles-public-api.module.ts
+++ libs/api/profiles/public/api/src/lib/api-profiles-public-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiProfilesPublicApiController } from './controllers/api-profiles-public-api-controller.controller';
+import { ApiProfilesPublicServiceModule } from '@grid-watch/api/profiles/public/service';
 
 @Module({
   controllers: [ApiProfilesPublicApiController],
-  imports:[], 
+  imports:[ApiProfilesPublicServiceModule], 
   providers: [],
   exports: [],
 })

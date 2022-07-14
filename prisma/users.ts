621560dda884c38704fd9b338137d2f74a5c5dc7
anru123 <anrunel.an@gmail.com>
2022-07-14 22:12:48 +0200

    🗃️ (db) : Added sseds for user

---
+++ prisma/users.ts
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        email : "pietersmith@gmail.com",
+        passsword : "PieterSmith@2222",
+        name    : "Pieter2222",
+    },
+    {
+        email : "johnnewman@gmail.com",
+        passsword : "J0hnN1955",
+        name    : "JohnnyBravo",
+    },
+    {
+        email : "drnyathi@hotmail.com",
+        passsword : "IamAI$1969",
+        name    : "Professor AI",
+    },
+    {
+        email : "billgates4231@outlook.com",
+        passsword : "iAMBILLGATE$1",
+        name    : "microsoftguy",
+    },    
+    {
+        email : "deniedaccess@gmail.com",
+        passsword : "Denied@access#21",
+        name    : "DeniedA",
+    }
+]

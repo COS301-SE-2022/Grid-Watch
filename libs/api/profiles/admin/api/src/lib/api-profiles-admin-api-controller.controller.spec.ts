41eefca4f50b09042cd76d31934e6006f7573e65
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-14 11:35:49 +0200

    🐛(api): Fix admin controller testing

--- libs/api/profiles/admin/api/src/lib/api-profiles-admin-api-controller.controller.spec.ts
+++ libs/api/profiles/admin/api/src/lib/api-profiles-admin-api-controller.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiProfilesAdminApiController } from './api-profiles-admin-api-controller.controller';
+import {ApiProfilesAdminService} from '@grid-watch/api/profiles/admin/service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 describe('ApiProfilesAdminApiController', () => {
   let controller: ApiProfilesAdminApiController;
@@ -7,14 +9,15 @@ describe('ApiProfilesAdminApiController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApiProfilesAdminApiController],
+      providers:[ApiProfilesAdminService,CommandBus,QueryBus]
     }).compile();
 
-    controller = module.get<ApiProfilesAdminApiController>(
-      ApiProfilesAdminApiController
-    );
+    await module.init();
+
+    controller = module.get<ApiProfilesAdminApiController>(ApiProfilesAdminApiController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-});
+})

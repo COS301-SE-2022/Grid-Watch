8b9eda57dedb69c32d5b73bc735a033560ea373e
anru123 <anrunel.an@gmail.com>
2022-07-14 22:28:08 +0200

    🐛 (db) : Fixed minor bug

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
 import {tickets} from './tickets';
 import {techteams} from './techteams';
 import {admins} from './admins';
+import {users} from './users';
+import {subtasks} from './subtasks';
 import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -21,6 +23,18 @@ async function main(){
     for (let admin of admins){
         await this.prisma.ticket.create({
             data: admin,
+        })
+    } 
+
+    for (let user of users){
+        await this.prisma.ticket.create({
+            data: user,
+        })
+    } 
+
+    for (let subtask of subtasks){
+        await this.prisma.ticket.create({
+            data: subtask,
         })
     } 
 

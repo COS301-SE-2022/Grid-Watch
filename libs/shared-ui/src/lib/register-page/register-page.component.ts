cd1a7c12d685370497665cdedfb7d29bedb7d9b7
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-16 17:21:24 +0200

    🐛(UI) : Register checks email to avoid duplicates

--- libs/shared-ui/src/lib/register-page/register-page.component.ts
+++ libs/shared-ui/src/lib/register-page/register-page.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { FloatLabelType } from '@angular/material/form-field';
 import { Router } from '@angular/router';
+import { UserDto } from '@grid-watch/api/profiles/public/api/shared/api-profiles-public-api-dto';
+import { PublicProfileService } from '../services/public-profile/public-profile.service';
 
 @Component({
   selector: 'grid-watch-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss'],
 })
-export class RegisterPageComponent {  //implements OnInit {
+export class RegisterPageComponent implements OnInit {
 
   
   hideRequiredControl = new FormControl(false);
@@ -18,36 +20,62 @@ export class RegisterPageComponent {  //implements OnInit {
     floatLabel: this.floatLabelControl,
   });
   
-  @Input() first_name! : string;
-  @Input() last_name! : string;
-  @Input() email! : string;
-  @Input() password! : string;
-  @Input() confirm_password! : string;
+  @Input() user! : UserDto;
+  @Input() confirmPassword! : string;
   hide = true;
   hideConfirm = true;
 
   constructor(
     private router : Router,
-    private formBuilder : FormBuilder
+    private formBuilder : FormBuilder, 
+    private profileService : PublicProfileService
   ) {}
 
-  // ngOnInit(): void {
+  ngOnInit(): void {
+    this.user = new UserDto();
+  }
 
-  // }
-
-  register() : void
+  async register() : Promise<void>
   {
-    console.log(this.first_name);
-    console.log(this.last_name);
-    console.log(this.email);
-    console.log(this.password);
-    console.log(this.confirm_password);
+    this.user.name = "Tshego";
+    this.user.email = "Tshego14@gmail.com";
+    this.user.password = "Gbfj&hfbsh";
+    this.confirmPassword = "Gbfj&hfbsh";
+    if (this.user.password === this.confirmPassword)
+    {
+     
+      console.log(await this.checkEmailExist());
+      
+      if (await this.checkEmailExist())
+      {
+        alert("Email already exists!")
+      }
+      else
+      {
+        this.profileService.createUser(this.user).subscribe(
+          () => {
+            alert("Created user successfully")
+          },
+          () => {
+            alert("Error while creating user")
+          }
+        );
+      }
+          
+      
+    }
+  }
+
+  async checkEmailExist() : Promise<boolean>
+  {
+    return await this.profileService.checkEmailExists(this.user.email);
     
   }
 
   back() : void {
     this.router.navigate(["/login",{app:"tech-team"}]);
   }
+
 
   getFloatLabelValue(): FloatLabelType {
     return this.floatLabelControl.value || 'auto';
---
+++ libs/shared-ui/src/lib/services/public-profile/public-profile.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserDto } from '@grid-watch/api/profiles/public/api/shared/api-profiles-public-api-dto';
+import { User } from '@prisma/client';
+import { catchError, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PublicProfileService {
+  private createUserURL = 'api/public/create';
+  private getUserEmailURL = 'api/public/email/';
+
+  constructor(private http: HttpClient) {}
+
+  public createUser(user: UserDto) {
+    return this.http
+      .post<UserDto>(this.createUserURL, user)
+      .pipe(catchError(this.handleError<UserDto>('createUser', new UserDto())));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.messgae}`);
+
+      return of(result as T);
+    };
+  }
+
+  public getUserEmail(email: string) : Observable<UserDto[]> {
+    const tempURL = this.getUserEmailURL + email;
+    return this.http
+      .get<UserDto[]>(tempURL)
+      .pipe(catchError(this.handleError<UserDto[]>('getEmails', [])));
+  }
+
+  public async checkEmailExists(email: string){
+    const users = await new Promise<UserDto[]>((resolve, reject)=>
+    {
+      this.getUserEmail(email).subscribe(
+        (res) =>
+        {
+          return resolve(res);
+        }
+      )
+    });
+
+    // console.log(users.length);
+    
+    if (users.length > 0)
+      return true;
+    else
+      return false;
+  } 
+}

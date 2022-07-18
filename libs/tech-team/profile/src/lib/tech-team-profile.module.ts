68a27f2be8b45e8e7c8d4222f403f6f686df360d
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-18 07:52:28 +0200

    ✨(UI) : Created Tech-Team login page

--- libs/tech-team/profile/src/lib/tech-team-profile.module.ts
+++ libs/tech-team/profile/src/lib/tech-team-profile.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { RegisterTechTeamComponent } from './register-tech-team/register-tech-team.component';
 import { FormsModule } from '@angular/forms';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild([
-      {path: 'registerTechTeam', pathMatch: 'full', component: RegisterTechTeamComponent} 
+      {
+        path: 'registerTechTeam',
+        pathMatch: 'full',
+        component: RegisterTechTeamComponent,
+      },
     ]),
   ],
-  declarations: [RegisterTechTeamComponent],
-  exports: [RegisterTechTeamComponent],
+  declarations: [RegisterTechTeamComponent, LoginPageComponent],
+  exports: [RegisterTechTeamComponent, LoginPageComponent],
 })
 export class TechTeamProfileModule {}

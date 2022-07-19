65f160e6f217ae37df3c0bb98506e970977f59b5
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-19 17:09:40 +0200

    ✨(UI) : Create admin Register page component

--- libs/admin-app/profile/src/index.ts
+++ libs/admin-app/profile/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/admin-app-profile.module';
+
+export * from './lib/login-page/login-page.component';
+
+export * from './lib/register-page/register-page.component';
--- libs/admin-app/profile/src/lib/admin-app-profile.module.ts
+++ libs/admin-app/profile/src/lib/admin-app-profile.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   imports: [
@@ -10,5 +12,7 @@ import { RouterModule } from '@angular/router';
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [LoginPageComponent, RegisterPageComponent],
+  exports: [LoginPageComponent, RegisterPageComponent],
 })
 export class AdminAppProfileModule {}

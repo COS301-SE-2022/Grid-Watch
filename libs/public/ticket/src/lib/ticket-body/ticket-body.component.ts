84c7527f7436b03df681942f3fc47dbcaa815352
Leonardo Wessels u17229457 <u17229457@tuks.co.za>
2022-07-19 11:51:12 +0200

    :lipstick: (UI): Modified issue card design:
    Changed ticket information as per meeting notes

--- libs/public/ticket/src/lib/ticket-body/ticket-body.component.ts
+++ libs/public/ticket/src/lib/ticket-body/ticket-body.component.ts
@@ -19,7 +19,9 @@ export class TicketBodyComponent implements OnInit {
   public name! : string;
   public surname! : string;
   public avatar! : string;
+  ticketImages : string[] = [];
   tickets : Array<TicketDto> = [];
+  
 
   constructor( private http: HttpClient,
               private ticketService: TicketService,
@@ -43,8 +45,7 @@ export class TicketBodyComponent implements OnInit {
           (response) => {
             this.InitialiseTicket(response)
           }
-        )
-      
+        )      
       }, (error) =>{console.log(error);
       });
 
@@ -58,11 +59,37 @@ export class TicketBodyComponent implements OnInit {
   async InitialiseTicket(data : TicketDto []) : Promise<void> 
   {
     for (let index = 0; index < data.length; index++) 
-    {
+    {      
       this.tickets.push(data[index]);
-      this.ticketService.getImages(data[index].ticketId).subscribe(
-        (response) => {
-          console.log(response);
+      
+      switch(this.tickets[index]["ticketType"])
+      {
+        case "Electricity Outage":
+          this.ticketImages.push("assets/issue-brokenpower.svg");
+          break;
+        case "Water Outage":
+          this.ticketImages.push("assets/issue-water.svg");
+          break;
+        case "Pothole":
+          this.ticketImages.push("assets/issue-pothole.svg");
+          break;
+        case "Sinkhole":
+          this.ticketImages.push("assets/issue-sinkhole.svg");
+          break;
+        case "Broken Traffic Light":
+          this.ticketImages.push("assets/issue-brokenrobot.svg");
+          break;
+        case "Broken Street Light":
+          this.ticketImages.push("assets/issue-brokenlight.svg");
+          break;
+        default:
+          this.ticketImages.push("assets/issue-maintenance.svg");
+          break;
+      }
+      this.ticketService.getImages(data[index].ticketId).subscribe
+      (
+        (response) => 
+        {
           if (response[response.length - 1])
             this.tickets[index].ticketImg = response[response.length - 1].pictureLink;
         }
@@ -73,8 +100,7 @@ export class TicketBodyComponent implements OnInit {
           this.tickets[index].ticketLocation = response;
         },
         (error) => {
-          console.log(error);
-          
+          console.log(error);          
         }
       );
     }

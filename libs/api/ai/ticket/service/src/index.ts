ffa8cacde10f14f8aad91f7cbad0503f05c724b9
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-20 16:19:20 +0200

    🐛 (ai): Fixed index.ts exports

--- libs/api/ai/ticket/service/src/index.ts
+++ libs/api/ai/ticket/service/src/index.ts
@@ -1,6 +1,4 @@
 export * from './lib/api-ai-ticket-service.service';
 export * from './lib/api-ai-ticket-service.module';
 export * from './lib/queries/api-ai-ticket-query-handler.handler';
-export * from './lib/commands/api-ai-ticket-handler.handler';
-export * from './lib/commands/api-ai-ticket-command.command';
 export * from './lib/queries/api-ai-ticket-query.query';

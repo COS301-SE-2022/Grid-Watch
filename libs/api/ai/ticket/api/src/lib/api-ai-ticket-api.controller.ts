75949de3384fbe0163329e1a23d7fecf49d21f34
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-20 10:35:20 +0200

    ✨ (api): Link ticket api controllers with services

--- libs/api/ai/ticket/api/src/lib/api-ai-ticket-api.controller.ts
+++ libs/api/ai/ticket/api/src/lib/api-ai-ticket-api.controller.ts
@@ -5,11 +5,12 @@ import { Body,
          Get, 
          //Param, 
          Post,} from '@nestjs/common';
+import {ApiAiTicketServiceService} from '@grid-watch/api/ai/ticket/service';
 
 @Controller('ticketAI')
 export class ApiAiTicketApiController {
 
-    // constructor(private readonly apiAiTicketApiService:ApiAiTicketApiService){}
+    constructor(private readonly apiAiTicketServiceService:ApiAiTicketServiceService){}
 
     //Testing endpoint to test functionality
     @Get()
@@ -20,19 +21,19 @@ export class ApiAiTicketApiController {
     //get endpiont to return the most relevant 
     @Post('/relevant')
     async getRelevantTechTeam(@Body() ticket:TicketDto){
-        //return this.apiTicketService.getRelevantTechTeam(ticket);
+        return this.apiAiTicketServiceService.getRelevantTechTeam(ticket);
     }
 
     //put endpoint to return estimated cost of this ticket
     @Post('estimate/cost')
     async getEstimateCost(@Body() ticket: TicketDto){
-        //return this.apiAiTicketService.getEstimateCost(ticket);
+        return this.apiAiTicketServiceService.getEstimateCost(ticket);
     }
 
     //put endpoint to return estimated time to complete a ticket
     @Post('estimate/time')
     async getEstimateTime(@Body() ticket: TicketDto){
-        //return this.apiAiTicketService.getEstimateTime(ticket);
+        return this.apiAiTicketServiceService.getEstimateTime(ticket);
     }
 
 }

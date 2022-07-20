ac77c6193d7daf9ac67d72e5422557f36a6ec5d6
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-20 16:10:03 +0200

    🐛 (ai): Fix module providers

--- libs/api/ai/ticket/service/src/lib/api-ai-ticket-service.module.ts
+++ libs/api/ai/ticket/service/src/lib/api-ai-ticket-service.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ApiAiTicketServiceService } from './api-ai-ticket-service.service';
-import {GetIssueAIHandler} from './queries/api-ai-ticket-query-handler.handler';
+import {GetIssueAIHandler,GetTechTeamSpecialisationHandler} from './queries/api-ai-ticket-query-handler.handler';
 import {ApiTicketRepositoryDataAccess} from '@grid-watch/api/ticket/repository';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import {ApiProfilesTechTeamRepositoryDataAccess} from '@grid-watch/api/profiles/tech-team/repository';
 @Module({
   controllers: [],
   imports: [CqrsModule],
   providers: [ApiAiTicketServiceService,
               ApiTicketRepositoryDataAccess,
-              GetIssueAIHandler],
+              ApiProfilesTechTeamRepositoryDataAccess,
+              GetIssueAIHandler,
+              GetTechTeamSpecialisationHandler],
   exports: [ApiAiTicketServiceService],
 })
 export class ApiAiTicketServiceModule {}

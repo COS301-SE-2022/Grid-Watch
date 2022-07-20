4871b5bfeacae8d2fbf1c90e39cc9d44f4b23df0
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-07-20 11:04:46 +0200

    🐛 (ai): Fix providers for queries

--- libs/api/ai/ticket/service/src/lib/api-ai-ticket-service.module.ts
+++ libs/api/ai/ticket/service/src/lib/api-ai-ticket-service.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ApiAiTicketServiceService } from './api-ai-ticket-service.service';
-
+import {GetIssueAIHandler} from './queries/api-ai-ticket-query-handler.handler';
+import {ApiTicketRepositoryDataAccess} from '@grid-watch/api/ticket/repository';
 @Module({
   controllers: [],
   imports: [CqrsModule],
-  providers: [ApiAiTicketServiceService],
+  providers: [ApiAiTicketServiceService,
+              ApiTicketRepositoryDataAccess,
+              GetIssueAIHandler],
   exports: [ApiAiTicketServiceService],
 })
 export class ApiAiTicketServiceModule {}
--- libs/api/ai/ticket/service/src/lib/api-ai-ticket-service.service.ts
+++ libs/api/ai/ticket/service/src/lib/api-ai-ticket-service.service.ts
@@ -1,20 +1,20 @@
 import { Injectable} from '@nestjs/common';
 import { TicketDto } from '@grid-watch/api/ticket/api/shared/ticketdto';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import {GetIssueQuery} from './queries/api-ai-ticket-query.query';
+import {GetIssueAIQuery} from './queries/api-ai-ticket-query.query';
 import {} from './commands/api-ai-ticket-command.command';
 @Injectable()
 export class ApiAiTicketServiceService {
-    constructor (private readonly commanndBus : CommandBus,
-                 private readonly queryBus : QueryBus){}
+    constructor (private commanndBus : CommandBus,
+                 private queryBus : QueryBus){}
 
     async getRelevantTechTeam(ticketDto: TicketDto){
         return "Working Progress";
     }
 
     async getEstimateCost(ticketDto: TicketDto){
-        let tickets:TicketDto[] = [];
-        tickets = await this.queryBus.execute(new GetIssueQuery(ticketDto.ticketType));
+        const tickets:TicketDto[] = [];
+        return await this.queryBus.execute(new GetIssueAIQuery(ticketDto.ticketType));
         
         let cost = 0.0;
         for(let i=0;i<tickets.length;i++){

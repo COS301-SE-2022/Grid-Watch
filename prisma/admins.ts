98f06058d099e190864169fe6c7260034366fc6a
anru123 <anrunel.an@gmail.com>
2022-07-20 21:56:14 +0200

    🌱 (db) : Added hashed passwords to seeds

--- prisma/admins.ts
+++ prisma/admins.ts
@@ -4,31 +4,31 @@ export const admins = [
         email             : "testadmin@gmail.com",
         contactNumber     : "0783397283",
         cities            : ["Pretoria,Johannesburg"],
-        password          : "@dmin#21",
-        passwordSalt      : ""
+        password          : "$2b$06$8mkuSpvlJUj/bTPeAqJyH.CspmfkOkEvpCRAbNZhf6CBY0M86uJpm", //@dmin#21
+        passwordSalt      : "$2b$06$8mkuSpvlJUj/bTPeAqJyH."
     },
     {
         name              : "Pieter Koen",
         email             : "pieterkoen@gmail.com",
         contactNumber     : "0783397282",
         cities            : ["Pretoria,Johannesburg"],
-        password          : "i@mpieterk0en21",
-        passwordSalt      : ""
+        password          : "$2b$06$Kb3YJZQww6jrz7NllghOP.KHskuLSrWPBTPYaq.UAb2cdSQZUr6Hm", //i@mpieterk0en21
+        passwordSalt      : "$2b$06$Kb3YJZQww6jrz7NllghOP."
     },
     {
         name              : "David Jordan",
         email             : "davidjordan1969@gmail.com",
         contactNumber     : "0728763392",
         cities            : ["Bloemfontein"],
-        password          : "DavidJordan#1969",
-        passwordSalt      : ""
+        password          : "$2b$06$tRjWVIfm7rqDX/SUNxFNT.HsCaQme0AGeJph1rOq0JXb3tYvQvYB6", //DavidJordan#1969
+        passwordSalt      : "$2b$06$tRjWVIfm7rqDX/SUNxFNT."
     },
     {
         name              : "Frederick South",
         email             : "fsouth1978@gmail.com",
         contactNumber     : "0812929930",
         cities            : ["Cape Town,Stellenbosch,Milnerton"],
-        password          : "F$outhGridWatch1",
-        passwordSalt      : ""
+        password          : "$2b$06$Mh190202KHoHHx9BbbVaNeh6ZZ9KPwNhqzMhNlRQX40lM8kfwz3Hi", //F$outhGridWatch1
+        passwordSalt      : "$2b$06$Mh190202KHoHHx9BbbVaNe"
     }
 ]
--- prisma/techteams.ts
+++ prisma/techteams.ts
@@ -6,8 +6,8 @@ export const techteams = [
         contactNumber     : "0223788832",
         nrJobsCompleted   : 5,
         ratingOfJobs      : 9.3,
-        password          : "TechTeam#1234",
-        passwordSalt      : ""
+        password          : "$2b$06$GWoHpvoMPJdpTWfhAZ5VXO5UEM9kedLgk2MVp/fcjlmsLp55Q..du",//TechTeam#1234
+        passwordSalt      : "$2b$06$GWoHpvoMPJdpTWfhAZ5VXO"
     },
     {
         name              : "Pikachu Electric",
@@ -16,8 +16,8 @@ export const techteams = [
         contactNumber     : "0223788839",
         nrJobsCompleted   : 5,
         ratingOfJobs      : 9.3,
-        password          : "1234Electricity",
-        passwordSalt      : ""
+        password          : "$2b$06$OtyvlKOwHhXomJA3lonHeOYtiKbdUSp6rpx2v9vYQ/3D3ayfJ/XcO",//1234#Electricity
+        passwordSalt      : "$2b$06$OtyvlKOwHhXomJA3lonHeO"
     },
     {
         name              : "The A team plumbers",
@@ -26,8 +26,8 @@ export const techteams = [
         contactNumber     : "0236789822",
         nrJobsCompleted   : 10,
         ratingOfJobs      : 9.8,
-        password          : "the@teamplumbers",
-        passwordSalt      : ""
+        password          : "$2b$06$URuWRTHi5N55b4D8UVOVyOH3HkWowTae4PzsVF92q8kGra9rgzbBe",//The@teamplumbers1
+        passwordSalt      : "$2b$06$URuWRTHi5N55b4D8UVOVyO"
     },
     {
         name              : "The Pothole Guys",
@@ -36,7 +36,7 @@ export const techteams = [
         contactNumber     : "0832339877",
         nrJobsCompleted   : 2,
         ratingOfJobs      : 7.3,
-        password          : "potholeguys4321",
-        passwordSalt      : ""
+        password          : "$2b$06$DhThS9tpS2dYNIAdcofrletFF3sQutp6r4HLDpScfuy9UCKuYgE7C",//Potholeguys#4321
+        passwordSalt      : "$2b$06$DhThS9tpS2dYNIAdcofrle"
     }
 ]

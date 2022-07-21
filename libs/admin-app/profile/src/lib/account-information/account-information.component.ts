a65970217edd31dc67a6d5151a255bc0de5776d4
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-21 15:09:18 +0200

    ✨(UI) : Account Information Component

---
+++ libs/admin-app/profile/src/lib/account-information/account-information.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grid-watch-account-information',
+  templateUrl: './account-information.component.html',
+  styleUrls: ['./account-information.component.scss'],
+})
+export class AccountInformationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

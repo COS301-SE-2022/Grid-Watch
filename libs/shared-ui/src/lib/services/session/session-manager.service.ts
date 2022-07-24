90053db959b0f4f6f75277dc1d3c68d2b2b31ebc
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-24 13:52:10 +0200

    ✨(UI) : Created session manager service

---
+++ libs/shared-ui/src/lib/services/session/session-manager.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionManagerService {
+
+  constructor() { }
+
+  login(userId : string)
+  {
+
+  }
+
+  logout()
+  {
+
+  }
+}

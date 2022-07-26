a54ebe3c599eed0663dfdcc4f7b5454ce2bb4d78
Anru Nel <anrunel.an@gmial.com>
2022-07-27 00:55:02 +0200

    🌱 (db) : Updated models and seeds

--- prisma/tickets.ts
+++ prisma/tickets.ts
@@ -12,7 +12,7 @@ export const tickets = [
         ticketDescription : "There is a large pothole in the middle of the road",    
         //ticketRepairTime :  55600,    
         ticketUpvotes : 0,   
-        userId  : 2
+        userId  : 1
     },
     {
         ticketStatus : "Opened",
@@ -27,22 +27,22 @@ export const tickets = [
         ticketDescription : "There is a power outage",    
         // ticketRepairTime :  55600,    
         ticketUpvotes : 0, 
-        userId  : 2
+        userId  : 1
     },
     {
         ticketStatus : "Opened",
-       // ticketCreateDate : "2022/07/11",
+        // ticketCreateDate : "2022/07/11",
         ticketType :  "Water",        
         ticketCity :  "Hatfield",            
         ticketLocation : "",      //place-id
         ticketLong   : -25.7534,
         ticketLat   : 28.2359,
-       // ticketStreetAddress: "1123 South St",
+        // ticketStreetAddress: "1123 South St",
         // ticketCost : 25000,           
-        ticketDescription : "A pipe has burst outside my house",    
+        ticketDescription : "The entire road is flooded ",    
         // ticketRepairTime :  55600,    
         ticketUpvotes : 0, 
-        userId  : 2
+        userId  : 1
     },
     {
         ticketStatus : "Closed",
@@ -54,11 +54,11 @@ export const tickets = [
         ticketLat   : 28.2359,
         //ticketStreetAddress: "1123 South St",
         // ticketCost : 25000,           
-        ticketDescription : "A pipe has burst outside my house", 
+        ticketDescription : "I have had no electricity for 2 days", 
         ticketCloseDate : "2022-07-22T17:38:27.870Z",  
         // ticketRepairTime :  55600,    
         ticketUpvotes : 0, 
-        userId  : 2
+        userId  : 1
     },
     {
         ticketStatus : "Opened",

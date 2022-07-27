011f0613c94af2e8d8dd40958ebd7a173b839128
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-07-27 14:24:56 +0200

    💄(UI) : Ticket clustering functional

--- libs/public/ticket/src/lib/ticket-body-map/ticket-body-map.component.ts
+++ libs/public/ticket/src/lib/ticket-body-map/ticket-body-map.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 import { TicketDto } from '@grid-watch/api/ticket/api/shared/ticketdto';
 import { GoogleMapsService, TicketService } from '@grid-watch/shared-ui';
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-
+// import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
+import { response } from 'Express';
 
 @Component({
   selector: 'grid-watch-ticket-body-map',
@@ -11,80 +12,86 @@ import { MarkerClusterer } from "@googlemaps/markerclusterer";
   styleUrls: ['./ticket-body-map.component.scss'],
 })
 export class TicketBodyMapComponent implements OnInit {
+  locations! : google.maps.LatLngLiteral [];
 
   map!: google.maps.Map;
-  zoom! : number;
-  center! : google.maps.LatLngLiteral
+  zoom!: number;
+  center!: google.maps.LatLngLiteral;
 
-
-  tickets! : TicketDto[];
-  markers!: google.maps.Marker []
+  tickets!: TicketDto[];
+  markers!: google.maps.Marker[];
   infoWindow!: google.maps.InfoWindow;
   constructor(
-    private googleMapsService : GoogleMapsService, 
-    private ticketService : TicketService
+    private googleMapsService: GoogleMapsService,
+    private ticketService: TicketService
   ) {}
 
   ngOnInit(): void {
+    this.locations = [];
     this.markers = [];
     const loader = new Loader({
-      apiKey: "AIzaSyDoV4Ksi2XO7UmYfl4Tue5JhDjKW57DlTE",
-      version: "weekly",
-      libraries: ["places"]
+      apiKey: 'AIzaSyDoV4Ksi2XO7UmYfl4Tue5JhDjKW57DlTE',
+      version: 'weekly',
+      libraries: ['places'],
     });
-    
-    loader.load().then(() => {
 
-      this.initMap();
-      
-      }, (error) =>{console.log(error);
+    loader.load().then(
+      () => {
+        this.initMap();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  initMap(): void {
+    this.ticketService.getTickets().subscribe(async (response) => {
+      this.tickets = response;
+      this.tickets.forEach((value) =>{
+        this.locations.push({lat : value.ticketLat, lng : value.ticketLong})
+      })
+      console.log(this.tickets);
+      const myLocation = await this.googleMapsService.getCurrentLocation();
+      const map = new google.maps.Map(
+        document.getElementById('mapContainer') as HTMLElement,
+        {
+          zoom: 7,
+          center: { lat: myLocation.latitude, lng: myLocation.longitude},
+        }
+      );
+
+      const infoWindow = new google.maps.InfoWindow({
+        content: '',
+        disableAutoPan: true,
       });
-  }
 
-  async initMap(){
-    this.zoom = 9;
-    const temp = await this.googleMapsService.getCurrentLocation();
-    console.log(temp);
-    
-    this.center =  {
-      lat: temp.latitude,
-      lng: temp.longitude,
-    };
-    this.map = this.googleMapsService.createMapObject("mapContainer",this.center,this.zoom)
-    
-    this.ticketService.getTickets().subscribe(
-      (response) =>
-      {
-        this.tickets = response;
-        console.log(response);
-        this.initialiseMarkers()
-      }
-    )
+      // Create an array of alphabetical characters used to label the markers.
+      const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    this.infoWindow = new google.maps.InfoWindow({
-      content: "",
-      disableAutoPan: true,
-    });
-
-    // marker.addListener("click", () => {
-    //   infoWindow.setContent(label);
-    //   infoWindow.open(map, marker);
-    // });
-  }
-
-  initialiseMarkers(){
-    for (let k = 0; k < this.tickets.length; k++) {
-      const pos = {
-        lat: this.tickets[k].ticketLat,
-        lng: this.tickets[k].ticketLong
-      }
-      this.markers.push(this.googleMapsService.createMarkerObject(pos, this.map, this.tickets[k].ticketType));
-      this.markers[k].addListener("click", () => {
-          this.infoWindow.setContent(this.tickets[k].ticketType);
-          this.infoWindow.open(this.map, this.markers[k]);
+      // Add some markers to the map.
+      const markers = this.locations.map((position, i) => {
+        // const label = this.tickets[i].ticketType.charAt(0);
+        const icon = "https://developers.google.com/maps/documentation/javascript/examples/full/images/library_maps.png";
+        const label = "";
+        const marker = new google.maps.Marker({
+          position,
+          label,
+          icon
         });
-    }
-    
-    new MarkerClusterer(this.map, this.markers);
+
+        // markers can only be keyboard focusable when they have click listeners
+        // open info window when marker is clicked
+        marker.addListener('click', () => {
+          infoWindow.setContent(this.tickets[i].ticketType);
+          infoWindow.open(map, marker);
+        });
+
+        return marker;
+      });
+
+      // Add a marker clusterer to manage the markers.
+      new MarkerClusterer({ markers, map });
+    });
   }
 }

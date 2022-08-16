b2736db928d0ae7220d2243404a3d55dbbb05338
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-08-16 17:50:16 +0200

    ✨ (ai): Complete plus node

--- libs/api/ai/ticket/service/src/lib/ai/plus-node.ts
+++ libs/api/ai/ticket/service/src/lib/ai/plus-node.ts
@@ -1,1 +1,24 @@
-export class PlusNode {}
+import { InternalNode } from "./internal-node";
+import { Node } from "./node";
+
+export class PlusNode extends InternalNode{
+    constructor(left : Node, right: Node){
+        super(left, right);
+    }
+
+    async clone() : Promise<Node> {
+        const out : Node = new PlusNode(await this.leftNode.clone(),await this.rightNode.clone());
+        out.setDepth(this.depth);
+        out.setFitness(await this.getFitness());
+        return out;
+    }
+
+    async getType() : Promise<string>{
+        return "plus";
+    }
+    
+    async execute() : Promise<number> {
+        return await this.leftNode.execute()+ await this.rightNode.execute();
+    }
+}
+

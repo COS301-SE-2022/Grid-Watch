8e745e04dc8a98752090c3e78316394cffa42e51
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-08-22 22:16:04 +0200

    🧪 (ai): Added failing test for gp

--- libs/api/ai/ticket/service/src/lib/ai/gp.spec.ts
+++ libs/api/ai/ticket/service/src/lib/ai/gp.spec.ts
@@ -1,7 +1,15 @@
-import { Gp } from './gp';
+import { GP } from './gp';
+import {Node} from './node';
 
-describe('Gp', () => {
-  it('should be defined', () => {
-    expect(new Gp()).toBeDefined();
+describe('GP', () => {
+const numbers = [[5,1,1,1,2,1,3,1,1],
+                [5,4,4,5,7,10,3,2,1],
+                [3,1,1,1,2,2,3,1,1],
+                [6,8,8,1,3,4,3,7,1],
+                [4,1,1,3,2,1,3,1,1]]
+  const expected = [0,0,0,0,0]
+  const test = new GP(200,5,30,numbers,expected);
+  it('Test whole GP', () => {
+    expect(test.GPA()).toEqual(5);
   });
 });

a25a096bf932ef5814b11c77dd0154e60d2272a9
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-08-23 20:16:05 +0200

    🧪 (ai): Added failing tests to div node spec

--- libs/api/ai/ticket/service/src/lib/ai/div-node.spec.ts
+++ libs/api/ai/ticket/service/src/lib/ai/div-node.spec.ts
@@ -1,7 +1,21 @@
+import { async } from 'rxjs';
 import { DivNode } from './div-node';
+import { MinNode } from './min-node';
+import { Node } from './node';
+import { PlusNode } from './plus-node';
 
 describe('DivNode', () => {
   it('should be defined', () => {
-    expect(new DivNode()).toBeDefined();
+    expect(new DivNode(null,null)).toBeDefined();
   });
+
+  it('Should be of type div',async ()=>{
+    const divNode = new DivNode(null,null);
+    expect(await divNode.getType()).toEqual("div");
+  })
+
+  it('Should be an exact clone',async ()=>{
+    const divNode : Node = new DivNode(new PlusNode(null,null),new MinNode(null,null));
+    expect(await divNode.clone()).toMatchObject(divNode);
+  })
 });

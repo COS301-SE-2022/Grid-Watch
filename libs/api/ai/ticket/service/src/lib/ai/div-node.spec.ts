bb9b3d8a42ef77406c2c72d4e2454bb625d798e7
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-08-23 20:37:19 +0200

    ✅ (ai): Added testing for div-node

--- libs/api/ai/ticket/service/src/lib/ai/div-node.spec.ts
+++ libs/api/ai/ticket/service/src/lib/ai/div-node.spec.ts
@@ -1,5 +1,5 @@
-import { async } from 'rxjs';
 import { DivNode } from './div-node';
+import { LeafNode } from './leaf-node';
 import { MinNode } from './min-node';
 import { Node } from './node';
 import { PlusNode } from './plus-node';
@@ -15,7 +15,22 @@ describe('DivNode', () => {
   })
 
   it('Should be an exact clone',async ()=>{
-    const divNode : Node = new DivNode(new PlusNode(null,null),new MinNode(null,null));
+    const divNode : Node = new DivNode(new PlusNode(new LeafNode(5),null),new MinNode(null,null));
     expect(await divNode.clone()).toMatchObject(divNode);
+  })
+
+  it('Clone tree without branches',async ()=>{
+    const divNode : Node = new DivNode(null,null);
+    expect(await divNode.clone()).toMatchObject(divNode);
+  })
+
+  it('Try executing tree with invalid division',async ()=>{
+    const divNode : Node = new DivNode(new LeafNode(15),new LeafNode(0))
+    expect(await divNode.execute()).toEqual(0);
+  })
+
+  it('Executing the tree',async() =>{
+    const divNode : Node = new DivNode(new PlusNode(new LeafNode(12),new LeafNode(5)),new MinNode(new LeafNode(-5),new LeafNode(5)));
+    expect(await divNode.execute()).toEqual(-1.7);
   })
 });

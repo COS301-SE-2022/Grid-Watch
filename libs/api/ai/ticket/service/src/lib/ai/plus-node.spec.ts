698c743f771db16bc4675ba3485d292690b787ff
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-08-24 13:05:40 +0200

    ✅ (ai): Added plus-Node testing

--- libs/api/ai/ticket/service/src/lib/ai/plus-node.spec.ts
+++ libs/api/ai/ticket/service/src/lib/ai/plus-node.spec.ts
@@ -1,7 +1,31 @@
+import { DivNode } from './div-node';
+import { LeafNode } from './leaf-node';
+import { multNode } from './mult-node';
+import { Node } from './node';
 import { PlusNode } from './plus-node';
 
 describe('PlusNode', () => {
   it('should be defined', () => {
-    expect(new PlusNode()).toBeDefined();
+    expect(new PlusNode(null,null)).toBeDefined();
   });
+
+  it('Should be of type min',async ()=>{
+    const plusNode = new PlusNode(null,null);
+    expect(await plusNode.getType()).toEqual("plus");
+  })
+
+  it('Should be an exact clone',async ()=>{
+    const plusNode : Node = new PlusNode(new DivNode(new LeafNode(5),null),new PlusNode(null,null));
+    expect(await plusNode.clone()).toMatchObject(plusNode);
+  })
+
+  it('Clone tree without branches',async ()=>{
+    const plusNode : Node = new PlusNode(null,null);
+    expect(await plusNode.clone()).toMatchObject(plusNode);
+  })
+
+  it('Executing the tree',async() =>{
+    const plusNode : Node = new PlusNode(new DivNode(new LeafNode(12),new LeafNode(5)),new multNode(new LeafNode(-5),new LeafNode(5)));
+    expect(await plusNode.execute()).toEqual(-22.6);
+  })
 });

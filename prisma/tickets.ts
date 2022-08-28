4cf737ad43fe3ab5f7a81aab899bae490cfd96b7
Anru Nel <anrunel.an@gmial.com>
2022-08-28 17:45:44 +0200

    🌱 (db) : Added entries to ticket seed

--- prisma/tickets.ts
+++ prisma/tickets.ts
@@ -103,48 +103,48 @@ export const tickets = [
         ticketUpvotes : 7, 
         userId  : 3
     },
-    // {
-    //     ticketStatus : "Opened",
-    //     ticketType :  "Broken Traffic Light",        
-    //     ticketCity :  "Johannesburg",            
-    //     ticketLocation : "ChIJoR6sngETlR4RQhq4aC3u_m4",      //place-id
-    //     ticketLong   : -26.126206, 
-    //     ticketLat   : 28.162290,
-    //     ticketStreetAddress: "38 Terrace Road",
-    //     // ticketCost : 25000,           
-    //     ticketDescription : "The traffic lights are not working", 
-    //     // ticketRepairTime :  55600,    
-    //     ticketUpvotes : 2, 
-    //     userId  : 3
-    // },
-    // {
-    //     ticketStatus : "Opened",
-    //     ticketType :  "Broken Traffic Light",        
-    //     ticketCity :  "Johannesburg",            
-    //     ticketLocation : "ChIJoR6sngETlR4RQhq4aC3u_m4",      //place-id
-    //     ticketLong   : -26.126206, 
-    //     ticketLat   : 28.162290,
-    //     ticketStreetAddress: "38 Terrace Road",
-    //     // ticketCost : 25000,           
-    //     ticketDescription : "The traffic lights are not working", 
-    //     // ticketRepairTime :  55600,    
-    //     ticketUpvotes : 2, 
-    //     userId  : 3
-    // },
-    // {
-    //     ticketStatus : "Opened",
-    //     ticketType :  "Broken Traffic Light",        
-    //     ticketCity :  "Johannesburg",            
-    //     ticketLocation : "ChIJoR6sngETlR4RQhq4aC3u_m4",      //place-id
-    //     ticketLong   : -26.126206, 
-    //     ticketLat   : 28.162290,
-    //     ticketStreetAddress: "38 Terrace Road",
-    //     // ticketCost : 25000,           
-    //     ticketDescription : "The traffic lights are not working", 
-    //     // ticketRepairTime :  55600,    
-    //     ticketUpvotes : 2, 
-    //     userId  : 3
-    // },
+    {
+        ticketStatus : "Opened",
+        ticketType :  "Pothole",        
+        ticketCity :  "Centurion",            
+        ticketLocation : "ChIJDconP2tmlR4R3Xulh0qe6Iw",      //place-id
+        ticketLong   : -25.877636, 
+        ticketLat   : 28.225392,
+        ticketStreetAddress: "11 Jan Smuts Ave",
+        // ticketCost : 25000,           
+        ticketDescription : "Potholes in the road right next to the Irene middle school.", 
+        // ticketRepairTime :  55600,    
+        ticketUpvotes : 2, 
+        userId  : 4
+    },
+    {
+        ticketStatus : "Opened",
+        ticketType :  "Pothole",        
+        ticketCity :  "Brakpan",            
+        ticketLocation : "EjBGbG9vcnMgU3QsIFdpdGhvayBFc3RhdGVzLCBCcmFrcGFuLCBTb3V0aCBBZnJpY2EiLiosChQKEgm3YYXiJCKVHhGXLSfgKNLCLRIUChIJ65RmVWohlR4R23Q3KsIjXRw",      //place-id
+        ticketLong   : -26.299496, 
+        ticketLat   : 28.334772,
+        ticketStreetAddress: "Heidelberg Road, R23",
+        // ticketCost : 25000,           
+        ticketDescription : "There are huge potholes on this main road", 
+        // ticketRepairTime :  55600,    
+        ticketUpvotes : 0, 
+        userId  : 1
+    },
+    {
+        ticketStatus : "Opened",
+        ticketType :  "Pothole",        
+        ticketCity :  "Pretoria",            
+        ticketLocation : "EidONCwgRGllIFdpbGdlcnMsIFByZXRvcmlhLCBTb3V0aCBBZnJpY2EiLiosChQKEgl1XI2CrgLAHhH1PJsxroNZORIUChIJMx9AODdelR4Rr8akWkFCPc8",      //place-id
+        ticketLong   : -25.753129, 
+        ticketLat   : 28.311490,
+        ticketStreetAddress: "N4, Die Wilgers, Pretoria",
+        // ticketCost : 25000,           
+        ticketDescription : "There are a lot of potholes on the highway, I almost hit one on my way home!", 
+        // ticketRepairTime :  55600,    
+        ticketUpvotes : 102, 
+        userId  : 5
+    },
     // {
     //     ticketStatus : "Opened",
     //     ticketType :  "Broken Traffic Light",        

c0a694597941cf7d6a9c35fe9668ddd4f298d985
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-09-04 19:11:23 +0200

    ✨(UI) : Created Tech Team body view

---
+++ libs/tech-team/ticket/src/lib/ticket-view-body/ticket-view-body.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grid-watch-ticket-view-body',
+  templateUrl: './ticket-view-body.component.html',
+  styleUrls: ['./ticket-view-body.component.scss'],
+})
+export class TicketViewBodyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

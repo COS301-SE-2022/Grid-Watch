bb4d82071de1e077a1bb58bdfde61b4491cb4e94
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-09-07 15:00:26 +0200

    🚨(UI) : Fixed failing tests because of missing import

--- libs/admin-app/tech-team/src/lib/tech-team-page/tech-team-page.component.spec.ts
+++ libs/admin-app/tech-team/src/lib/tech-team-page/tech-team-page.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TechTeamPageComponent } from './tech-team-page.component';
@@ -9,6 +10,7 @@ describe('TechTeamPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TechTeamPageComponent],
+      imports: [HttpClientModule]
     }).compileComponents();
   });
 

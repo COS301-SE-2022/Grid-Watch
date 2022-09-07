154b37a66727c804cf6666a7cd848eded51a25f9
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-09-07 11:50:26 +0200

    🔥 (ai): Correctly remove unnecessary workspaces

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { ApiTicketApiModule } from '@grid-watch/api/ticket/api';
 import { ApiProfilesTechTeamApiModule } from '@grid-watch/api/profiles/tech-team/api';
 import { ApiProfilesPublicApiModule } from '@grid-watch/api/profiles/public/api';
 import { ApiProfilesAdminApiModule } from '@grid-watch/api/profiles/admin/api';
-import { ApiAiAdminApiModule} from '@grid-watch/api/ai/admin/api';
 import {ApiAiTicketApiModule} from '@grid-watch/api/ai/ticket/api';
-import {ApiAiNotificationsApiModule} from '@grid-watch/api/ai/notifications/api';
 import { Module } from '@nestjs/common';
 
 
 @Module({
-  imports: [ApiTicketApiModule,ApiProfilesTechTeamApiModule,ApiProfilesPublicApiModule,ApiProfilesAdminApiModule,ApiAiAdminApiModule,ApiAiTicketApiModule,ApiAiNotificationsApiModule],
+  imports: [ApiTicketApiModule,ApiProfilesTechTeamApiModule,ApiProfilesPublicApiModule,ApiProfilesAdminApiModule,ApiAiTicketApiModule,],
   controllers: [],
   providers: [],
 })

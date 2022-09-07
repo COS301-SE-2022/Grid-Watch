1e0a8b24cb137e0a2770e3f5fb2fb2cb3dfa857b
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-09-07 11:22:10 +0200

    🔥 (ai): remove unnecessary workspaces

--- libs/api/ai/admin/api/src/lib/api-ai-admin-api.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('api-ai-admin-api')
-export class ApiAiAdminApiController {}
--- libs/api/ai/notifications/api/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/api-ai-notifications-api.controller';
-export * from './lib/api-ai-notifications-api.module';

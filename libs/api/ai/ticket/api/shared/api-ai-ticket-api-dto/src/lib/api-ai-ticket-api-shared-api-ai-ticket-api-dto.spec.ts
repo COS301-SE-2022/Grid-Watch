8551882f2b2e0a6dc3f39489d67130665a964186
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-09-13 15:51:11 +0200

    ✨ (ai): Create ai dto

---
+++ libs/api/ai/ticket/api/shared/api-ai-ticket-api-dto/src/lib/api-ai-ticket-api-shared-api-ai-ticket-api-dto.spec.ts
@@ -0,0 +1,9 @@
+import { apiAiTicketApiSharedApiAiTicketApiDto } from './api-ai-ticket-api-shared-api-ai-ticket-api-dto';
+
+describe('apiAiTicketApiSharedApiAiTicketApiDto', () => {
+  it('should work', () => {
+    expect(apiAiTicketApiSharedApiAiTicketApiDto()).toEqual(
+      'api-ai-ticket-api-shared-api-ai-ticket-api-dto'
+    );
+  });
+});

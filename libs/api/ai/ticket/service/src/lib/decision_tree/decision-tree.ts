149d7208a9f02eb8e145b800c5a8aae581b3855b
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-09-21 14:33:17 +0200

    🎨 (ai): Added helper and getBestSplit functions

--- libs/api/ai/ticket/service/src/lib/decision_tree/decision-tree.ts
+++ libs/api/ai/ticket/service/src/lib/decision_tree/decision-tree.ts
@@ -58,4 +58,71 @@ export class DecisionTree {
         return new Node(0,0,null,null,0,leaf_value);        
     }
 
+    private getFeatureValues(dataset:number[][],featureIndex:number){
+        const features:number[]=[];
+        for(let i=0;i<dataset.length;i++){
+            for(let j=0;j<dataset[i].length;j++){
+                if(j==featureIndex){
+                    features.push(dataset[i][j]);
+                }
+            }
+        }
+        return features;
+    }
+
+    private getUnique(featureValues:number[]){
+        const unique:number[] = [];
+        for(let i=0;i<featureValues.length;i++){
+            let bfound = false;
+            for(let j=0;j<unique.length;j++){
+                if(featureValues[i]==unique[i]){
+                    bfound=true;
+                }
+            }
+
+            if(!bfound){
+                unique.push(featureValues[i]);
+            }
+        }
+        return unique;
+    }
+
+    get_best_split(dataset, num_samples, num_features){
+    
+        const best_split:BestSplit = new BestSplit();
+        let max_var_red = -Infinity;
+
+        for(let feature_index=0;feature_index<num_features;feature_index++){
+            const feature_values:number[] = this.getFeatureValues(dataset,feature_index);
+            const possible_thresholds:number[] = this.getUnique(feature_values);
+            
+            for(let t=0;t<possible_thresholds.length;t++){
+                const threshold = possible_thresholds[t];
+
+                const dataset_left = this.splitleft(dataset,feature_index,threshold);
+                const dataset_right = this.splitright(dataset,feature_index,threshold);
+                
+                if(dataset_left.length > 0 && dataset_right.length > 0){
+                    const y = this.getExpected(dataset);
+                    const left_y = this.getExpected(dataset_left);
+                    const right_y = this.getExpected(dataset_right);
+                    
+                    
+                    const curr_var_red = this.variance_reduction(y, left_y, right_y);
+                    
+                    if (curr_var_red > max_var_red){
+                        best_split.setFeatureIndex(feature_index);
+                        best_split.setThreshold(threshold);
+                        best_split.setDatasetLeft(dataset_left);
+                        best_split.setDatasetRight(dataset_right);
+                        best_split.setVarRed(curr_var_red);
+                        max_var_red = curr_var_red;
+                    }
+                }
+            }
+                        
+        }
+        return best_split
+    }
+
 }

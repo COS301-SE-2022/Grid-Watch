68201fd53dbb792ef1f1b06686ab5a96f6e06803
Mr Joshua Young(u20442018) <u20442018@tuks.co.za>
2022-09-21 14:31:24 +0200

    🎨 (ai): Attributes and constructor to tree

--- libs/api/ai/ticket/service/src/lib/decision_tree/decision-tree.ts
+++ libs/api/ai/ticket/service/src/lib/decision_tree/decision-tree.ts
@@ -2,5 +2,14 @@ import { BestSplit } from "./best-split";
 import { Node } from "./node";
 
 export class DecisionTree {
+    root:Node;
+    min_samples_split:number;
+    max_depth:number;
+    constructor(min_samples_split:number,max_depth:number){
+        this.root = null;
+
+        this.min_samples_split = min_samples_split;
+        this.max_depth = max_depth;
+    }
     
 }

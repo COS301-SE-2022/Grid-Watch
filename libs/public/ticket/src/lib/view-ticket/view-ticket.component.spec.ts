cb51a1cf4bfde4c4141193481c440da390c3c1fe
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-09-27 21:52:23 +0200

    🚨(UI) : Fixed failing tests

--- libs/public/ticket/src/lib/view-ticket/view-ticket.component.spec.ts
+++ libs/public/ticket/src/lib/view-ticket/view-ticket.component.spec.ts
@@ -5,33 +5,36 @@ import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
- '@grid-watch/environments';
-
+('@grid-watch/environments');
 
 import { ViewTicketComponent } from './view-ticket.component';
+import { GoogleMapsService } from '@grid-watch/shared-ui';
 // import { GoogleMapsService } from 'libs/shared-ui/src/lib/services/GoogleMaps/google-maps.service';
 
 describe('ViewTicketComponent', () => {
   let component: ViewTicketComponent;
   let fixture: ComponentFixture<ViewTicketComponent>;
-  let GoogleMapsService : any
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ViewTicketComponent],
-      imports : [HttpClientModule, RouterTestingModule, MatMenuModule,
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+        MatMenuModule,
         AngularFireModule,
         AngularFireModule.initializeApp({
-    apiKey: "AIzaSyB5VKBU78lomWEVrtiUOYIglrM2VdC0jI8",
-    authDomain: "epi-use-c9dfa.firebaseapp.com",
-    projectId: "epi-use-c9dfa",
-    storageBucket: "epi-use-c9dfa.appspot.com",
-    messagingSenderId: "342205045804",
-    appId: "1:342205045804:web:b401e9bde4216d489a8589",
-    measurementId: "G-JW453VRZD1"
-  }),
-        AngularFirestoreModule,],
-      providers: [GoogleMapsService]
+          apiKey: 'AIzaSyB5VKBU78lomWEVrtiUOYIglrM2VdC0jI8',
+          authDomain: 'epi-use-c9dfa.firebaseapp.com',
+          projectId: 'epi-use-c9dfa',
+          storageBucket: 'epi-use-c9dfa.appspot.com',
+          messagingSenderId: '342205045804',
+          appId: '1:342205045804:web:b401e9bde4216d489a8589',
+          measurementId: 'G-JW453VRZD1',
+        }),
+        AngularFirestoreModule,
+      ],
+      providers: [GoogleMapsService],
     }).compileComponents();
   });
 
